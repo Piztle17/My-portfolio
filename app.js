6f3fad22fecd81a9f1af6a2e41fdd5f5f3301a0f
Piztle17 <mustaphaabiola17@gmail.com>
2023-01-10 06:02:24 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector (".flex_code");
+const showcontent = document.querySelector (".progress_br");
+
+btn.addEventListener
+btn.addEventListener("click", function() {
+        showcontent.classList.toggle("active");
+    })
