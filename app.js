b9ccf41ddf14193c75c92c04f9cbae75b3dc0905
Piztle17 <mustaphaabiola17@gmail.com>
2023-01-16 04:22:35 -0600

    update on profile

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const btn = document.querySelector (".flex_code");
 const showcontent = document.querySelector (".progress_br");
+const arrow = document.querySelector(".arrow");
+const slider = document.querySelector (".sidenav_bar");
+const opat = document.querySelectorAll (".textt");
+// const hoverwd = document.querySelectorAll(".hoveer_bg");
+const label = document.querySelector (".label");
+const bodyclr = document.querySelector(".body");
+const btncoll = document.querySelectorAll ("button")
 
-btn.addEventListener
-btn.addEventListener("click", function() {
-        showcontent.classList.toggle("active");
+arrow.addEventListener("click", ()=>{
+    slider.classList.toggle("closee");
+
+    opat.forEach(item => {
+        item.classList.toggle("texttt");
     })
+
+})
+
+// btn.addEventListener
+// btn.addEventListener("click", function() {
+//         showcontent.classList.toggle("active");
+//     })
+
+label.addEventListener("click", ()=>{
+    bodyclr.classList.toggle("bg_mmnav");
+    btncoll.forEach(btt =>{
+        btt.classList.toggle("btncolor")
+    })
+})
